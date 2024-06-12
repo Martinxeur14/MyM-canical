6619dec24ba1aa6df720844d199dca6133d40b07
Martinxeur14 <martin.programme14@gmail.com>
2024-06-12 19:52:58 +0200

    Version 1

--- Mytest.cs
+++ Mytest.cs
@@ -17,7 +17,7 @@ namespace MyTest
         public override void OnPluginInit()
         {
            base.OnPluginInit();
-            Debug.Log("MyTest est initialisé avec succès !");
+            Debug.Log("MyMécanical est initialisé avec succès !");
         }
 
         public override void OnPlayerInput(Player player, KeyCode keyCode, bool onUI)
@@ -26,18 +26,19 @@ namespace MyTest
             
             if(keyCode == KeyCode.F7)
             {
-                player.SendText("Menu Mécanical");
-
                 player.Notify("MyMécanical", "Seulement les Employé on le droit le lancer se menu");
 
-                UIPanel panel = new UIPanel("MyTest", UIPanel.PanelType.Text);
+                UIPanel panel = new UIPanel("MyTest", UIPanel.PanelType.Tab);
+                UIPanel panel2 = new UIPanel("Contacte", UIPanel.PanelType.Tab);
                 panel.SetTitle("Menu Mécanical");
                 panel.text = "Ceci est le menu pour les employés de Mécanical. Ce menu est en développement";
                 panel.AddButton("Fermer", ui => player.ClosePanel(ui));
-                panel.AddButton("Contacter le personnel", ui => player.Notify("MyMécanical", "Vous avez contacté le personnel"));
+                panel.AddTabLine("Contacter le personnel", (ui) => player.Notify("MyMécanical", "Vous avez contacté le personnel"));
+                panel2.AddTabLine("Louis Peche", (ui) => player.Notify("Téléphone", "Le numéro de téléphone est 010203040506"));
 
                 player.ShowPanelUI(panel);
+                player.ShowPanelUI(panel2);
             }
         }
     }
-}
+} 
